bc6204ed63bbd0d9285cf95be1eea6af350a6c29
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 11:23:35 +0330

    implemented hospital_settings migration

--- database/migrations/2024_04_19_091049_create_hospital_settings_table.php
+++ database/migrations/2024_04_19_091049_create_hospital_settings_table.php
@@ -13,6 +13,46 @@ return new class extends Migration
     {
         Schema::create('hospital_settings', function (Blueprint $table) {
             $table->id();
+            //basic info
+            $table->string("name")->nullable();
+            $table->string("website")->nullable();
+            $table->string("phone")->nullable();
+            $table->string("fax")->nullable();
+            $table->string("country")->nullable();
+            $table->string("address")->nullable();
+            $table->date("establish")->nullable();
+            $table->string("email")->unique()->nullable();
+            $table->string("logo")->nullable();
+            $table->string("favicon")->nullable();
+            $table->string("size")->nullable();
+            $table->string("type")->nullable();
+            $table->string("facebook")->nullable();
+            $table->string("twitter")->nullable();
+            $table->string("whatsapp")->nullable();
+            $table->string("instagram")->nullable();
+
+            // mail divers config
+            $table->string("driver")->nullable();
+            $table->string("encryption")->nullable();
+            $table->string("host")->nullable();
+            $table->string("port")->nullable();
+            $table->string("username")->nullable();
+            $table->string("email_from")->nullable();
+            $table->string("email_from_name")->nullable();
+
+            // invoice setting config
+            $table->string("invoice_prefix")->nullable();
+            $table->string("invoice_log")->nullable();
+            $table->string("user_prefix")->nullable();
+            $table->string("patient_prefix")->nullable();
+            $table->string("invoice_number_mood")->nullable();
+            $table->string("invoice_test_number")->nullable();
+
+            // tax config
+            $table->string("taxes")->default(0);
+            $table->string("discount")->default(0);
+
+
             $table->timestamps();
         });
     }
