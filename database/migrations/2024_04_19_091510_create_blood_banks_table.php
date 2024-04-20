15cb679234dcd936509fc99c0cd3e38e97f05ad1
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 14:45:15 +0330

    implemented blood_banks table migration

--- database/migrations/2024_04_19_091510_create_blood_banks_table.php
+++ database/migrations/2024_04_19_091510_create_blood_banks_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('blood_banks', function (Blueprint $table) {
             $table->id();
+            $table->string("blood_type");
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(Patient::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
