4e0d72a2def2f416ab0686cc6bd2bce678913d05
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 11:36:05 +0330

    implemented branches table migration

--- database/migrations/2024_04_19_091133_create_branches_table.php
+++ database/migrations/2024_04_19_091133_create_branches_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,8 +13,16 @@ return new class extends Migration
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
 
+            $table->string("name")->fulltext();
+            $table->text("address")->fulltext();
+            $table->string("phone")->fulltext();
+            $table->string("email")->unique()->fulltext();
+            $table->string("website")->unique()->fulltext()->nullable();
 
-
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
