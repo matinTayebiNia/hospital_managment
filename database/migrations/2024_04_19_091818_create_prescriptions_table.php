4ec7ff113402ed9acf42a1e0ea1e1ed79cd112d7
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 19:08:08 +0330

    implemented prescriptions table migration

--- database/migrations/2024_04_19_091818_create_prescriptions_table.php
+++ database/migrations/2024_04_19_091818_create_prescriptions_table.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Models\Medicine;
+use App\Models\Patient;
+use App\Models\PatientVisit;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +16,26 @@ return new class extends Migration
     {
         Schema::create('prescriptions', function (Blueprint $table) {
             $table->id();
+            $table->string("dosage")->nullable();
+            $table->string("frequency")->nullable();
+            $table->string("duration")->nullable();
+            $table->string("food_relation")->nullable();
+            $table->string("route")->nullable();
+            $table->string("instruction")->nullable();
+
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(Patient::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(PatientVisit::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(Medicine::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
