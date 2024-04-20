fe70557da844a10616c489d5b69a3d7c1aa82118
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 14:41:36 +0330

    implemented       ->foreignIdFor(User::class)->constrained()

---
+++ app/Http/Controllers/LaboratoriesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Laboratories;
+use App\Http\Requests\StoreLaboratoriesRequest;
+use App\Http\Requests\UpdateLaboratoriesRequest;
+
+class LaboratoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLaboratoriesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Laboratories $laboratories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Laboratories $laboratories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLaboratoriesRequest $request, Laboratories $laboratories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Laboratories $laboratories)
+    {
+        //
+    }
+}
--- database/migrations/2024_04_19_091448_create_laboratories_table.php
+++ database/migrations/2024_04_19_091448_create_laboratories_table.php
@@ -1,27 +1,19 @@
 <?php
 
-use App\Models\Patient;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('schedules', function (Blueprint $table) {
+        Schema::create('laboratories', function (Blueprint $table) {
             $table->id();
-            $table->time("start_time")->nullable();
-            $table->time("end_time")->nullable();
+            $table->string("name");
             $table->tinyInteger("status")->default(0);
-            $table->string("available_days")->nullable();
-            $table->string("note")->nullable();
-            $table->foreignIdFor(User::class)->constrained()
-                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId("created_by_id")->nullable()->constrained("users")
                 ->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId("updated_by_id")->nullable()->constrained("users")
@@ -35,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('schedules');
+        Schema::dropIfExists('laboratories');
     }
 };
