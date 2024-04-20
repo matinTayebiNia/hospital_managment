89cf32104defbcf9bcf4f943f632df97e030fe8c
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 11:41:23 +0330

    implemeted specialist table migration

--- database/migrations/2024_04_19_091224_create_specialists_table.php
+++ database/migrations/2024_04_19_091224_create_specialists_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,15 @@ return new class extends Migration
     {
         Schema::create('specialists', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->fulltext();
+            $table->string("code")->fulltext();
+            $table->tinyInteger("status")->default(0)->comment("1 = active , 0 = none");
+
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
