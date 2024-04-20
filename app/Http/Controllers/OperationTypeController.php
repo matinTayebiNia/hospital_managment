489f529274c146a90c3503c0358b3a278ee7f6c4
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 18:46:23 +0330

    implemented operation table migration and create all needed file for Operation type model and implemented operation type table migration

---
+++ app/Http/Controllers/OperationTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OperationType;
+use App\Http\Requests\StoreOperationTypeRequest;
+use App\Http\Requests\UpdateOperationTypeRequest;
+
+class OperationTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOperationTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOperationTypeRequest $request, OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OperationType $operationType)
+    {
+        //
+    }
+}
--- database/migrations/2024_04_19_091532_create_operations_table.php
+++ database/migrations/2024_04_19_091532_create_operations_table.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\OperationType;
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +15,21 @@ return new class extends Migration
     {
         Schema::create('operations', function (Blueprint $table) {
             $table->id();
+            $table->date("operations_date");
+            $table->time("operations_time");
+            $table->integer("amount");
+            $table->text("description")->nullable();
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(OperationType::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(Patient::class)->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()
+                ->onDelete("null")->cascadeOnUpdate();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
