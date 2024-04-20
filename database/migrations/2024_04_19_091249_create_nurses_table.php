82fc3c709ff4152e499db83bde58490584bcf6c7
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 12:00:09 +0330

    implemented nurse table migration

--- database/migrations/2024_04_19_091249_create_nurses_table.php
+++ database/migrations/2024_04_19_091249_create_nurses_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Specialist;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('nurses', function (Blueprint $table) {
             $table->id();
+            $table->string("about_nurse")->nullable();
+            $table->string("experience")->nullable();
+            $table->foreignIdFor(Specialist::class)->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
