2771463fefaa86b60f9185d5afce691103204612
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 11:57:56 +0330

    implemented doctor table migraion

--- database/migrations/2024_04_19_091237_create_doctors_table.php
+++ database/migrations/2024_04_19_091237_create_doctors_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Specialist;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,12 +14,13 @@ return new class extends Migration {
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
-            $table->string("title")->fulltext()->comment(" such as Mr. Dr");
-            $table->string("name");
             $table->string("about_doctor")->nullable();
-            $table->string("about_doctor")->nullable();
-            $table->string("about_doctor")->nullable();
-
+            $table->integer("charge")->default(0);
+            $table->string("experience")->nullable();
+            $table->foreignIdFor(Specialist::class)->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId("created_by_id")->nullable()->constrained("users")
                 ->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId("updated_by_id")->nullable()->constrained("users")
