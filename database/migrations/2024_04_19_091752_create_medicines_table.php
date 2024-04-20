7e4761eb2ad0981736192954a7165ab657c5b9f2
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 19:03:56 +0330

    implemented medicine table migration

--- database/migrations/2024_04_19_091752_create_medicines_table.php
+++ database/migrations/2024_04_19_091752_create_medicines_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Purchase;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
+            $table->string("medicine_code")->nullable();
+            $table->string("medicine_name")->nullable();
+            $table->integer("medicine_price")->default(0);
+            $table->text("description")->nullable();
+            $table->integer("available_qty")->default(0);
+            $table->integer("alert_qty")->default(0);
+
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(Purchase::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+          $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
