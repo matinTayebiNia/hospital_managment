7fa91b6455815fa74a4a3928e74af7b0d25546e1
matinTayebiNia <matintayebinia@gmail.com>
2024-04-20 19:00:14 +0330

    implemented purchase table migration

--- database/migrations/2024_04_19_091732_create_purchases_table.php
+++ database/migrations/2024_04_19_091732_create_purchases_table.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\MedicineCategory;
+use App\Models\MedicineType;
+use App\Models\Supplier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +15,35 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
+            $table->string("code");
+            $table->string("name");
+            $table->tinyInteger("type");
+
+            //medicines
+            $table->string("medicine_generic_name")->nullable();
+            $table->string("medicine_unit")->nullable();
+            $table->string("medicine_strength")->nullable();
+            $table->string("medicine_shelf")->nullable();
+
+            // quantity and price
+            $table->string("quantity")->default(0);
+            $table->string("quantity_type")->nullable();
+            $table->string("price")->nullable();
+            $table->date("expiration_date")->nullable();
+            $table->string("note")->nullable();
+            $table->string("image")->nullable();
+
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(MedicineType::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(MedicineCategory::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(Supplier::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
