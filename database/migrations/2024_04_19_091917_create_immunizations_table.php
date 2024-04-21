418f1ee2760bbd402db07801d720a2a5431203ab
matinTayebiNia <matintayebinia@gmail.com>
2024-04-21 09:41:18 +0330

    implemented allergies and immunizations table migration

--- database/migrations/2024_04_19_091917_create_immunizations_table.php
+++ database/migrations/2024_04_19_091917_create_immunizations_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Patient;
+use App\Models\PatientVisit;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,18 @@ return new class extends Migration
     {
         Schema::create('immunizations', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->nullable();
+            $table->tinyInteger("status")->default(0);
+            $table->foreignIdFor(Patient::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(PatientVisit::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained()
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("created_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId("updated_by_id")->nullable()->constrained("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
